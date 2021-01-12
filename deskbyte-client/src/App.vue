df708e1d07fcb7d44b3a4b343d94f993efbd1ec2
natan <natan.hart1@gmail.com>
2021-01-12 21:31:16 +0200

    Header basics

--- deskbyte-client/src/App.vue
+++ deskbyte-client/src/App.vue
@@ -1,11 +1,25 @@
 <template>
-  <div id="nav">
-    <img alt="logo" src="../src/assets/logo.png" id="logo" />
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-    <router-view />
+  <div>
+    <Header />
+    <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
+    <div class="router-view">
+      <router-view />
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Header from "./components/Header.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Header,
+  },
+});
+</script>
 
 <style lang="scss">
 #app {
@@ -13,15 +27,10 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  background-color: #fffce8;
-  height: 100vh;
-  margin: 0px;
+
 }
 
-#logo {
-  height: 1rem;
-}
+
 
 #nav {
   padding: 30px;
---
+++ deskbyte-client/src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header">
+    <div class="logo">
+      <img alt="logo" src="../assets/logo.png" id="logo" />
+    </div>
+    <div class="auth">
+      <h3>Login</h3>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Header",
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: $db-navy;
+  height: 10vh;
+  padding: 0.7rem 2rem 0.7rem 2rem;
+}
+.logo {
+  margin-top: -0.2rem;
+  #logo {
+    height: 3.8rem;
+  }
+}
+
+.auth {
+  margin-right: 2rem;
+}
+
+// ul {
+//   list-style-type: none;
+//   padding: 0;
+// }
+// li {
+//   display: inline-block;
+//   margin: 0 10px;
+// }
+</style>
